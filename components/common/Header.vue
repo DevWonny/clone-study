95073118ee0f8f63e7013c3485112222525b8a80
Kevin <kevin@b2ggames.com>
2023-02-18 23:38:26 +0900

    feat : header 컴포넌트 퍼블리싱 진행중

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -4,6 +4,27 @@
       <div class="header-logo">
         <img src="~/static/header/header-logo.png" alt="Logo" />
       </div>
+
+      <div class="menu-warp">
+        <ul class="util-menu">
+          <li>Sign In</li>
+          <li>My Starbucks</li>
+          <li>Customer Service &amp; Ideas</li>
+          <li>Find a Store</li>
+        </ul>
+
+        <div class="search-container">search</div>
+
+        <ul class="main-menu">
+          <li>COFFEE</li>
+          <li>MENU</li>
+          <li>STORE</li>
+          <li>RESPONSIBILITY</li>
+          <li>STARBUCKS REWARDS</li>
+          <li>CORPORATE SALES</li>
+          <li>WHAT'S NEW</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -16,11 +37,28 @@
   height: 120px;
   border: 1px solid red;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
 
   .header-container {
     width: 1100px;
     height: 100%;
     background: red;
+
+    .header-logo {
+      width: 75px;
+      height: 75px;
+
+      &img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .menu-wrap {
+      width: 895px;
+      height: 100%;
+    }
   }
 }
 </style>
